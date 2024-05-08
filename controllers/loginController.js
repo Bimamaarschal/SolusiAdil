4e7887a06c51d37ef4eaf0c9510e98afccebb0e3
Bimamaarschal <bimamaarschal@gmail.com>
2024-05-08 09:23:10 +0700

    dassda

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,24 +1,23 @@
 const axios = require("axios");
 
 exports.loginUser = async (req, res) => {
-    try {
-      const { nik, password } = req.body;
-      const response = await axios.post(
-        "https://solusiadil-api.vercel.app/users/login",
-        { nik, password }
-      );
-      req.session.user = response.data;
-  
-      const userDataResponse = await axios.get(`https://solusiadil-api.vercel.app/users/nik/${nik}`);
-      const userData = userDataResponse.data;
-  
-      req.session.userData = userData;
-  
-      res.redirect("/beranda");
-    } catch (error) {
-      res.status(400).send(error.response.data);
-    }
-  };
+  try {
+    const { nik, password } = req.body;
+    const response = await axios.post(
+      "https://solusiadil-api.vercel.app/users/login",
+      { nik, password }
+    );
+    req.session.user = response.data;
+    const userDataResponse = await axios.get(`https://solusiadil-api.vercel.app/users/nik/${nik}`);
+    const userData = userDataResponse.data;
+
+    req.session.userData = userData;
+
+    res.redirect("/beranda");
+  } catch (error) {
+    res.status(400).send(error.response.data);
+  }
+};
 
 exports.logoutUser = (req, res) => {
   req.session.destroy((err) => {
