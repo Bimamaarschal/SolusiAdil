5798ce003b5c3f82005d395ee8e275d183ec3b66
Bimamaarschal <bimamaarschal@gmail.com>
2024-05-14 12:37:54 +0700

    dasdas

--- public/function/beranda.js
+++ public/function/beranda.js
@@ -21,3 +21,42 @@ async function fetchUserData() {
     }
 }
 document.addEventListener('DOMContentLoaded', fetchUserData);
+
+{/* <form class="form" method="post" action="mail/mail.php">
+									<div class="row">
+										<div class="col-lg-6 col-md-6 col-12">
+											<div class="form-group">
+												<i class="fa fa-user"></i>
+												<input type="text" name="first-name" placeholder="Nama Lengkap"
+													required="required">
+											</div>
+										</div>
+										<div class="col-lg-6 col-md-6 col-12">
+											<div class="form-group">
+												<i class="fa fa-envelope"></i>
+												<input type="email" name="email" placeholder="Your Email"
+													required="required">
+											</div>
+										</div>
+										<div class="col-12">
+											<div class="form-group">
+												<i class="fa-solid fa-scale-balanced"></i>
+												<input type="text" name="last-name" placeholder=" Pengajuan Pertanyaan"
+													required="required">
+											</div>
+										</div>
+										<div class="col-12">
+											<div class="form-group message">
+												<i class="fa fa-pencil"></i>
+												<textarea name="message" rows="7"
+													placeholder="keterangan Lengkap"></textarea>
+											</div>
+										</div>
+										<div class="col-12">
+											<div class="form-group button">
+												<button type="submit" class="btn primary"><i
+														class="fa fa-send"></i>Ajukan Konsultasi</button>
+											</div>
+										</div>
+									</div>
+								</form> */}
--- routes/routeRoutes.js
+++ routes/routeRoutes.js
@@ -11,8 +11,35 @@ function checkLoggedIn(req, res, next) {
     }
 }
 
-router.get('/tentangkami',  (req, res) => {
+
+
+router.get('/tentangkami', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'tentangkami.html'));
+});
+
+router.get('/blog',  (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'blog.html'));
+});
+
+router.get('/konsultasi',  (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'konsultasi.html'));
+});
+
+router.get('/layanan', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'layanan.html'));
+});
+
+router.get('/kontak', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'kontak.html'));
+});
+
+
+router.get('/faq', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'faq.html'));
+});
+
+router.get('/gabung', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'gabung.html'));
 });
 
 module.exports = router;
