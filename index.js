36790b6962e3904e4380670bf5e55bad6fabc4f2
Bimamaarschal <bimamaarschal@gmail.com>
2024-05-14 10:40:47 +0700

    dasdas

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use('/', express.static('public'));
 app.use('/', registerRoutes);
 app.use('/', loginRoutes);
 
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
